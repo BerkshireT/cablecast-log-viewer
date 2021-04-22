ce7b1006803daa47e56558a6bd1004b9878a41ff
Tyler <berkshiret122@gmail.com>
2021-04-22 14:34:31 -0400

    basic data flow
    file selection and passing logs to the child component are complete

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Cablecast logo" src="./assets/logo.png" width="250px">
+    <h1>Cablecast Log Viewer</h1>
+
+    <input type="file" accept=".log" @change="loadLogs">
+
+    <LogViewer v-if="logsLoaded" :logs="logs"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LogViewer from './components/LogViewer.vue'
 
 export default {
+  components: { LogViewer },
   name: 'App',
-  components: {
-    HelloWorld
+  data() { return {
+    logsLoaded: false,
+    logs: [],
+  }},
+  methods: {
+    loadLogs (event) {
+      var file = event.target.files[0]
+      var reader = new FileReader()
+      var self = this // need this so the closure can assign data to vue object
+
+      reader.onload = (function(reader) {
+        return function() {
+          var contents = reader.result
+          var lines = contents.split('\n')
+          self.logs = lines
+          self.logsLoaded = true
+        }
+      })(reader)
+      
+      try {
+        reader.readAsText(file)
+      }
+      catch (err) { // exiting out of the file popup throws an error, quick fix
+        console.log(err.message)
+      }
+    },
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/LogViewer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="LogViewer">
+     <h3>{{ logs.length }} </h3>
+    <ul>
+      <li v-for="(log, index) in logs" :key="index">{{index}} - {{log}}</li>
+    </ul>
+    <h3>Filters</h3>
+    <h3>Date Range</h3>
+    <h3>Custom Search</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LogViewer',
+  props: {
+     logs: {
+        type: Array,
+        required: true
+     }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

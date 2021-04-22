735669c09229f783559b4a0711703c90effcde88
Tyler <berkshiret122@gmail.com>
2021-04-22 16:45:21 -0400

    severity filters
    severity filters and button changing done. can only have one filter on at a time.

--- src/components/LogViewer.vue
+++ src/components/LogViewer.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="LogViewer">
     <h3>Filters</h3>
+    <div id="filterButtons">
+      <button @click="InfoFilter" v-bind:class="infoFilter ? 'green' : 'default'">INFO</button>
+      <button @click="WarningFilter" v-bind:class="warningFilter ? 'green' : 'default'">WARN</button>
+      <button @click="ErrorFilter" v-bind:class="errorFilter ? 'green' : 'default'">ERROR</button>
+    </div>
     <h3>Date Range</h3>
     <h3>Custom Search</h3>
     <tr>
-      <td>Timestamp</td>
-      <td>Severity</td>
-      <td>Message</td>
+      <td id="label">Timestamp</td>
+      <td id="label">Severity</td>
+      <td id="label">Message</td>
     </tr>
-    <tr v-for="(log, index) in logs" :key="index">
+    <tr v-for="(log, index) in computeLogs" :key="index">
       <td>{{ log.timestamp }}</td>
       <td>{{ log.severity }}</td>
       <td>{{ log.message }}</td>
@@ -28,11 +33,46 @@ export default {
      }
   },
   data() { return {
-
+     infoFilter: false,
+     warningFilter: false,
+     errorFilter: false,
+     noResults: [ { timestamp: '', severity: '', message: 'no results found'} ]
   }},
   methods: {
-     test () {
-        filters.foo()
+     InfoFilter () {
+        this.infoFilter = !this.infoFilter
+        this.warningFilter = false
+        this.errorFilter = false
+     },
+     WarningFilter () {
+        this.infoFilter = false
+        this.warningFilter = !this.warningFilter
+        this.errorFilter = false
+     },
+     ErrorFilter () {
+        this.infoFilter = false
+        this.warningFilter = false
+        this.errorFilter = !this.errorFilter
+     }
+  },
+  computed: {
+     computeLogs() {
+        var filteredLogs
+        if (this.infoFilter) {
+           filteredLogs = filters.info(this.logs)
+        } else if (this.warningFilter) {
+           filteredLogs = filters.warning(this.logs)
+        } else if (this.errorFilter) {
+           filteredLogs = filters.error(this.logs)
+        } else {
+           filteredLogs = this.logs
+        }
+
+        if (filteredLogs.length == 0) {
+           return this.noResults
+        } else {
+           return filteredLogs
+        }
      }
   }
 }
@@ -48,5 +88,28 @@ ul {
 td {
    padding-top: 10px;
    padding-bottom: 10px;
+}
+
+button {
+   margin: 5px 20px 5px 20px;
+}
+
+#filterButtons {
+   display: inline-block;
+}
+
+#label {
+   padding-left: 10px;
+   padding-right: 10px;
+}
+
+.default {
+   
+}
+
+.green {
+   color: white;
+   background-color: #2CB466;
+   border-color: #2CB466;
 }
 </style>
--- src/scripts/filterMethods.js
+++ src/scripts/filterMethods.js
@@ -1,5 +1,39 @@
 var filters = {
-   foo() { console.log('hit foo')}
+   info(logs) { 
+      var filteredLogs = []
+
+      logs.forEach(function(line) {
+         if (line.severity == "INFO") {
+            filteredLogs.push(line)
+         }
+      })
+
+      return filteredLogs
+   },
+
+   warning(logs) { 
+      var filteredLogs = []
+
+      logs.forEach(function(line) {
+         if (line.severity == "WARN") {
+            filteredLogs.push(line)
+         }
+      })
+
+      return filteredLogs
+   },
+
+   error(logs) { 
+      var filteredLogs = []
+
+      logs.forEach(function(line) {
+         if (line.severity == "ERROR") {
+            filteredLogs.push(line)
+         }
+      })
+
+      return filteredLogs
+   }
 }
 
 export default filters
